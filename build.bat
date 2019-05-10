866e145bb2f00154e95da6e9a5a8a135d6abd787
Laurent Wang <lwang@btib.fr>
2019-05-10 11:28:35 +0200

    Adding Honeywell Excel10 device description

--- build.bat
+++ build.bat
@@ -1,6 +1,18 @@
 @echo off
 
+::%1%
+set home=C:\Niagara\Niagara-4.7.109.20
+
+echo Set user env:
+SETX NIAGARA_HOME "%home%"
+SETX -m NIAGARA_HOME "%home%"
+
 :: build script
 call %~dp0gradlew.bat build
 call %~dp0gradlew.bat --stop
 
+echo Remove user env:
+reg delete "HKEY_CURRENT_USER\Environment" /v NIAGARA_HOME /f
+reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v NIAGARA_HOME /f
+
+PAUSE
